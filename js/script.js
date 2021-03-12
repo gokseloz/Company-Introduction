1ec9b39715f10bd80c95874e47cb8ef488746f8b
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-12 15:36:38 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ let memberBtn = document.querySelectorAll(".member-btn")
 let customerBtn = document.querySelectorAll(".customer-btn")
 let customer = document.querySelectorAll(".customer")
 
+let inputs = document.querySelectorAll(".inputs")
+
+let successText = document.querySelector(".form-successText")
+let warningText = document.querySelector(".form-warningText")
+
+
+let contactForm = document.querySelector(".contact-form")
+let inputName = document.querySelector(".form-name")
+let inputEmail = document.querySelector(".form-email")
+let textArea = document.querySelector(".form-textArea")
+
 memberBtn.forEach(btn => {
     btn.addEventListener("click", () => {
         btn.classList.toggle("active")
@@ -15,3 +26,41 @@ customerBtn.forEach(btn => {
         btn.parentElement.parentElement.classList.toggle("rotate")
     })
 })
+
+
+
+
+
+const warning = () => {
+    warningText.style.display = "block"
+    setTimeout(() => {
+        warningText.style.display = "none"
+    }, 3000);
+}
+
+
+const logSubmit = (e) => {
+    e.preventDefault()
+
+    if (inputName.value == "" || inputEmail.value == "" || textArea.value == "") {
+        warning()
+
+    } else {
+        successText.style.display = "block"
+        inputs.forEach(input => {
+            input.value = ""
+        })
+
+        setTimeout(() => {
+            successText.style.display = "none"
+        }, 3000);
+    }
+}
+
+
+inputEmail.addEventListener("invalid", (e) => {
+    e.preventDefault()
+    warning()
+})
+
+contactForm.addEventListener("submit", logSubmit)
