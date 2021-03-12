d26a5aee2d1cd39ad8997ae925bf02fe782d6c6e
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-12 13:39:47 +0100

    responsive adding

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 let memberBtn = document.querySelectorAll(".member-btn")
+let customerBtn = document.querySelectorAll(".customer-btn")
+let customer = document.querySelectorAll(".customer")
 
 memberBtn.forEach(btn => {
     btn.addEventListener("click", () => {
         btn.classList.toggle("active")
-        btn.parentNode.children[4].classList.toggle("active") 
+        btn.parentNode.children[4].classList.toggle("active")
     })
 });
+
+
+customerBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.parentElement.parentElement.classList.toggle("rotate")
+    })
+})
