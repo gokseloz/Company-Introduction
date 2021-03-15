4a194985ee60e272b59d2826be51a4151ff90188
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-15 10:33:24 +0100

    components adding

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let memberBtn = document.querySelectorAll(".member-btn")
+let memberBtn = document.querySelectorAll(".team-member-btn")
 let customerBtn = document.querySelectorAll(".customer-btn")
 let customer = document.querySelectorAll(".customer")
 
