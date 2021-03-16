c04f21a7fca996f719f81a2e581895bc395870af
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-16 13:17:41 +0100

    refactor: add namedItem method

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ customerBtn.forEach(btn => {
 memberBtn.forEach(btn => {
     btn.addEventListener("click", () => {
         btn.classList.toggle("active")
-        btn.parentNode.children[4].classList.toggle("active")      
+        btn.parentNode.children.namedItem("team-member-aboutMe").classList.toggle("active")      
     })
 });
 
